96f91dee00600ec3d4109db5b9016b9715a9f542
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-13 20:34:35 +0300

    get method with requests

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from decouple import config
+import requests
 
 def index(request):
+    API_KEY = config("API_KEY")
+    city = "Eskisehir"
+    url =f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+    response = requests.get(url)
+    print(response) #! 👉 <Response [200]>
     return render(request, 'weatherapp/index.html')
