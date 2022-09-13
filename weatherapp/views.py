0679331d44de957b613863a07ff43b4a73a941ad
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-13 20:41:18 +0300

    for more readable print.. imported pprint

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from decouple import config
 import requests
+from pprint import pprint
 
 def index(request):
     API_KEY = config("API_KEY")
-    city = "Eskisehir"
+    city = "Ankara"
     url =f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
     response = requests.get(url)
+    content = response.json()
     print(response) #! 👉 <Response [200]>
+    pprint(content)
+
     return render(request, 'weatherapp/index.html')
