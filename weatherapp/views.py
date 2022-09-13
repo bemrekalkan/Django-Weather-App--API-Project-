43b761cd1c01bbfdd9a82eb3ba913c956834903a
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-13 20:51:23 +0300

    context added

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -8,8 +8,20 @@ def index(request):
     city = "Ankara"
     url =f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
     response = requests.get(url)
+    #? We converted the incoming data to python dictionary format 👇
     content = response.json()
     print(response) #! 👉 <Response [200]>
-    pprint(content)
+    # pprint(content)
+    pprint(content["name"])
+    pprint(content["main"]["temp"])
+    pprint(content["weather"][0]["description"])
+    pprint(content["weather"][0]["icon"])
 
-    return render(request, 'weatherapp/index.html')
+    context = {
+        "city": content["name"],
+        "temp": content["main"]["temp"],
+        "icon": content["weather"][0]["icon"],
+        "desc": content["weather"][0]["description"]
+    }
+
+    return render(request, 'weatherapp/index.html', context)
